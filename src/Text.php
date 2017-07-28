2c1fa5346e60a2648e2081b751442a5a3c1cf031
Leo Ding <leoding86@msn.com>
2017-07-28 18:07:01 +0800

    update Remove some exceptions and add comment

--- src/Text.php
+++ src/Text.php
@@ -6,6 +6,20 @@ use InvalidArgumentException;
 use OutOfBoundsException;
 use BadMethodCallException;
 
+/**
+ * This class is for creating text object for adding to image
+ * 
+ * @property string   $text
+ * @property string   $font  Local font file
+ * @property callable $pen   A function or method for adding text to image
+ * @property Color    $color A color object
+ * @property mixed    $size  Font size
+ * @property mixed    $angle Text angle
+ * @property array    $box   Text box, include width and height
+ * @property callable $bbox  A function or method for get text bounding box
+ * 
+ * @author Leo Ding <leoding86@msn.com>
+ */
 class Text
 {
   private $text;
@@ -17,6 +31,15 @@ class Text
   private $box;
   private $bbox;
 
+  /**
+   * Construct a text
+   *
+   * @param string $text
+   * @param mixed  $size
+   * @param mixed  $angle
+   * @param Color  $color
+   * @param string $font
+   */
   public function __construct($text, $size, $angle, Color $color, $font)
   {
     $this->setText($text)
@@ -38,7 +61,7 @@ class Text
     if (property_exists($this, $property)) {
       return $this->$property;
     } else {
-      throw new RuntimeException('Unkown property {Color::' . $property . '}');
+      trigger_error('Unkown property {Color::' . $property . '}', E_USER_ERROR);
     }
   }
 
@@ -52,10 +75,27 @@ class Text
       }
     }
 
-    throw new BadMethodCallException('Unkown setter {Color::' . $setter . '}');
+    trigger_error('Unkown setter {Color::' . $setter . '}', E_USER_ERROR);
   }
 
+  /**
+   * The alias of calculateTextBoxSize for compatible old version
+   *
+   * @return void
+   */
   public function getTextBoundingBox()
+  {
+    $this->calculateTextBoxSize();
+  }
+
+  /**
+   * Calculate text box size
+   * 
+   * Remember to call this method after reset text, size, angle, font to get new text box size
+   *
+   * @return void
+   */
+  public function calculateTextBoxSize()
   {
     $angle = $this->angle % 180;
 
@@ -74,6 +114,12 @@ class Text
     }
   }
 
+  /**
+   * Setter of property size
+   *
+   * @param mixed $size
+   * @return void
+   */
   public function setSize($size)
   {
     if (is_int($size) || is_float($size)) {
@@ -84,6 +130,12 @@ class Text
     }
   }
 
+  /**
+   * Setter of property text
+   *
+   * @param mixed $text
+   * @return void
+   */
   public function setText($text)
   {   
     if (is_string($text) && !empty($text)) {
@@ -94,6 +146,12 @@ class Text
     }
   }
 
+  /**
+   * Setter of property font
+   *
+   * @param  string $font
+   * @return void
+   */
   public function setFont($font)
   {
     if (is_file($font)) {
@@ -115,6 +173,12 @@ class Text
     return $this;
   }
 
+  /**
+   * Setter of propery angle
+   *
+   * @param  mixed $angle
+   * @return void
+   */
   public function setAngle($angle)
   {
     if (is_int($angle) || is_float($angle)) {
@@ -125,12 +189,26 @@ class Text
     }
   }
 
+  /**
+   * Setter of propery color
+   *
+   * @param  Color $color
+   * @return void
+   */
   public function setColor(Color $color)
   {
     $this->color = $color;
     return $this;
   }
 
+  /**
+   * Append current text to a image
+   *
+   * @param Image $image
+   * @param int   $x
+   * @param int   $y
+   * @return void
+   */
   public function writeTo(Image $image, $x, $y)
   {
     $image->write($this, $x, $y);
